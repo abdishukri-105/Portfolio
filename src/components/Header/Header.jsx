8f322146e2fcce669a6472558dd27b68dc3a2276
abdishukri <abdishukrimohamed105@gmail.com>
2023-06-14 20:17:31 +0300

    working on nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,37 @@ const Header = () => {
             <div className="flex items-center justify-between">
                 {/* logo */}
                 <div className="flex items-center gap-[10px]">
-                    <span className="w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center">A</span>
+                    <span className="w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center">AM</span>
 
-
+                    <div className="leading-[20px]">
+                        <h2 className="text-xl text-smallTextColor font-[700]">Abdishukri</h2>
+                        <p className="text-smallTextColor text-[16px] font-[500]">Mohamed</p>
+                    </div>
                 </div>
 
-            </div>
+                           {/* ==========logo end====== */}
+                           {/* ===========menu start ====== */}
 
+                <div className="menu">
+                    <ul className="flex items-center gap-10">
+                        <li>
+                            <a href="#about">About</a>
+                        </li>
+                        <li>
+                            <a href="#services">Services</a>
+                        </li>
+                        <li>
+                            <a href="#portfolio">Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+
+                    </ul>
+    
+                </div>           
+
+            </div>
         </div>
     </header>
 }
