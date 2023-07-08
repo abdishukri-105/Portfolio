a075e8e33db23654e53243dd471a1fcce4b3640f
abdishukri <abdishukrimohamed105@gmail.com>
2023-07-08 20:43:36 +0300

    fixed bug

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = ({activeID, setShowModal}) => {
     const portfolio = portfolios.find(portfolio => portfolio.id === activeID);
 
     return (
-        <div className="w-full h-1/2 fixed top-40 left-0 z-10 bg-headingColor bg-opacity-0 " >
+        <div className="w-full h-full fixed top-0 left-0 z-10 bg-headingColor bg-opacity-0 " >
            <div className=" md:max-w-[600px] md:w-full  absolute top-1/2 left-1/2 z-20 bg-white rounded-[8px] transform -translate-x-1/2 -translate-y-1/2 p-5">
              <div>
                 <figure>
